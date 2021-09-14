91cba528cf7471410732658fb22a2eee6d827321
hexiandong <hexiandong@linklogis.com>
2021-09-14 18:35:35 +0800

    函数柯里化、模拟new

---
+++ js手写/js 函数柯里化.js
@@ -0,0 +1,37 @@
+// function curry(fn, ...args) {
+//   function currid(...args2) {
+//     const allArgs = [...args, ...args2];
+//     return curry.call(null, fn, ...allArgs);
+//   }
+//   currid.toString = function () {
+//     return fn.apply(null, args)
+//   }
+//   return  currid
+// }
+function dynamicAdd(...args) {
+  return [...args].reduce((prev, curr) => {
+    return prev + curr;
+  }, 0)
+}
+//
+//
+// const add = curry(dynamicAdd);
+// alert((add(1)(2)(3,4)));
+
+
+// 参数固定写法
+function  curry(fn,...args1){
+  if(fn.length===args1.length){
+    return  fn(...args1)
+  }
+  return  function (...args2){
+    return curry(fn,...args1,...args2)
+  }
+}
+function add2(a,b){
+  return a+b;
+}
+
+const add = curry(dynamicAdd);
+console.log(add);
+console.log(add(1));
---
+++ js手写/模拟new.js
@@ -0,0 +1,19 @@
+function mockNew(fn, ...args) {
+  if (typeof fn !== 'function') {
+    throw new Error('fn is not Function')
+  }
+  // 创建一个继承fn原型的对象
+  const newObj = Object.create(fn.prototype);
+  // 将fn中的也就是构造函数中的this指向newObj,并继承其属性，且接收其返回结果
+  const result = fn.apply(newObj, args);
+  return result && (typeof result === 'object' || typeof result === 'function') ? result : newObj
+}
+
+function Person(name, age) {
+  this.name = name;
+  this.age = age; // new 操作符如果构造函数没有return，则会自动加上return this,
+  // 也可以主动更改如 return {},则this指向这个空对象
+}
+
+let person = mockNew(Person, '张三', 18);
+console.log(person);
